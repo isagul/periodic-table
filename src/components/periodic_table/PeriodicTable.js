f9f6e8111fd49ff2158948c12ab6f8bb93044d02
Isa Gul <isa.gul@beyn.com.tr>
2019-12-05 18:58:41 +0300

    color classes were added

--- src/components/periodic_table/PeriodicTable.js
+++ src/components/periodic_table/PeriodicTable.js
@@ -4,42 +4,42 @@ import './PeriodicTable.scss';
 
 const PeriodicTable = () => {
     const { state } = useContext(Store);
-    
+
     const elementButton = (startIndex, endIndex) => {
         return (
             state.elements.length > 0 &&
             state.elements.slice(startIndex, endIndex).map((element, index) => {
                 return <button key={index}>{element.name_small}</button>
-            })      
-        )      
+            })
+        )
     }
 
     return (
         <div className="periodic-table-component">
             <div className="periodic-table">
-                { elementButton(0, 2) }
-            </div>
-            <div className="periodic-table">
-                <div> 
-                    { elementButton(2, 4) }
-                </div>
-                <div>
-                    { elementButton(4, 10) }
-                </div>
+                {elementButton(0, 2)}
             </div>
             <div className="periodic-table">
                 <div>
-                    { elementButton(10, 12) }
+                    {elementButton(2, 4)}
                 </div>
                 <div>
-                    { elementButton(12, 18) }
-                </div>                
+                    {elementButton(4, 10)}
+                </div>
             </div>
             <div className="periodic-table">
-                { elementButton(18, 90) }
+                <div>
+                    {elementButton(10, 12)}
+                </div>
+                <div>
+                    {elementButton(12, 18)}
+                </div>
+            </div>
+            <div className="periodic-table">
+                {elementButton(18, 90)}
             </div>
             <div className="periodic-table-footer">
-                { elementButton(90, 120) }
+                {elementButton(90, 120)}
             </div>
 
         </div>
