77fd6b2d748c92897340bd47f41464e5960e780f
Isa Gul <isa.gul@beyn.com.tr>
2020-03-06 15:26:36 +0300

    Updates

---
+++ precache-manifest.2ac8f49c98aaae1a7f0e661db111dd39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "470b104a1a3541a4438dcf7bd3d86749",
+    "url": "/periodic-table/index.html"
+  },
+  {
+    "revision": "e33a61c812dc40a7c760",
+    "url": "/periodic-table/static/css/main.eb1afb64.chunk.css"
+  },
+  {
+    "revision": "dde850b2d9311430a9b4",
+    "url": "/periodic-table/static/js/2.270e2c74.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/periodic-table/static/js/2.270e2c74.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e33a61c812dc40a7c760",
+    "url": "/periodic-table/static/js/main.796e4967.chunk.js"
+  },
+  {
+    "revision": "985effa055f8299554c1",
+    "url": "/periodic-table/static/js/runtime-main.706d903e.js"
+  }
+]);
